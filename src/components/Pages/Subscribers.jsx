de8f6942266f543dd1672f74f8f2e632ee81eff0
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-04-28 23:35:41 +0530

    added Subscribers & Subscriptions page

--- src/components/Pages/Subscribers.jsx
+++ src/components/Pages/Subscribers.jsx
@@ -1,5 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL_PREFIX } from "../../constants";
+
 const Subscribers = () => {
-  return <div>Subscribers</div>;
+  const [subscribers, setSubscribers] = useState(null);
+
+  if (!localStorage.getItem("isLoggedIn")) {
+    return "Please log in to see your subscribers";
+  }
+
+  const fetchSubscribers = async () => {
+    const res = await axios.get(
+      `${BACKEND_URL_PREFIX}/subscriptions/subscribers`,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(res);
+    setSubscribers(res.data.data);
+  };
+
+  useEffect(() => {
+    fetchSubscribers();
+  }, []);
+
+  if (!subscribers) {
+    return "loading...";
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl my-3">Subscribers</h1>
+      <div className="flex flex-wrap">
+        {subscribers.map((subscriber) => (
+          <div className="bg-gray-300 rounded-md flex py-2 px-3 items-center justify-center mr-2" key={subscriber._id}>
+            <div><img className="w-10 h-10 rounded-full mr-3" src={subscriber.subscribers[0].avatar} alt="avatar"/></div>
+            <div>{subscriber.subscribers[0].username}</div>
+          </div>   
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Subscribers;
--- src/components/Pages/Subscriptions.jsx
+++ src/components/Pages/Subscriptions.jsx
@@ -1,5 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL_PREFIX } from "../../constants";
+
 const Subscriptions = () => {
-  return <div>Subscriptions</div>;
+  const [subscriptions, setSubscriptions] = useState(null);
+
+  if (!localStorage.getItem("isLoggedIn")) {
+    return "Please log in to see your subscriptions";
+  }
+
+  const fetchSubscriptions = async () => {
+    const res = await axios.get(
+      `${BACKEND_URL_PREFIX}/subscriptions/subscribedTo`,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(res);
+    setSubscriptions(res.data.data);
+  };
+
+  useEffect(() => {
+    fetchSubscriptions();
+  }, []);
+
+  if (!subscriptions) {
+    return "loading...";
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl my-3">Subscriptions</h1>
+      <div className="flex">
+        {subscriptions.map((subscription) => (
+          <div className="bg-gray-300 rounded-md flex py-2 px-3 items-center justify-center mr-2" key={subscription._id}>
+            <div><img className="w-10 h-10 rounded-full mr-3" src={subscription.channels[0].avatar} alt="avatar"/></div>
+            <div>{subscription.channels[0].username}</div>
+          </div>   
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Subscriptions;
