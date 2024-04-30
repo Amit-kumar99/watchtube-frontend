e4c7bb3dc970f43d3255fcfbc437f7957c8d9622
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-04-30 22:10:28 +0530

    added tweets page & refactored backend

--- src/components/Pages/Tweets.jsx
+++ src/components/Pages/Tweets.jsx
@@ -1,5 +1,88 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL_PREFIX } from "../../constants";
+import { useParams } from "react-router-dom";
+import LikedIcon from "../../../assets/likedIcon.png";
+import LikeIcon from "../../../assets/likeIcon.png";
+
 const Tweets = () => {
-  return <div>Tweets</div>;
+  const { channelId } = useParams();
+  const [tweets, setTweets] = useState(null);
+
+  const fetchTweets = async () => {
+    const res = await axios.get(
+      `${BACKEND_URL_PREFIX}/tweets/get/${channelId}`,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(res.data.data);
+    if (res.data.statusCode === 200) {
+      setTweets(res.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  const handleEditTweet = () => {};
+
+  const handleDeleteTweet = () => {};
+
+  const handleToggleTweetLike = () => {};
+
+  if (!tweets) {
+    return "loading...";
+  }
+
+  return (
+    <div className="mb-3">
+      {tweets.map((tweet) => (
+        <div key={tweet._id} className="border w-8/12 flex p-5 rounded-md mb-5">
+          <div className="mr-3">
+            <img
+              className="w-10 h-10 rounded-full"
+              src={tweet.owner[0].avatar}
+            />
+          </div>
+
+          <div>
+            <div>
+              <span className="mr-2 font-semibold">
+                {tweet.owner[0].username}
+              </span>
+              <span>{tweet.createdAt} ago</span>
+            </div>
+
+            <div>{tweet.content}</div>
+
+            <div className="flex">
+              <span className="mr-1">{tweet.likesCount}</span>
+              <span onClick={handleToggleTweetLike}>
+                <img className="w-5 cursor-pointer" src={tweet.isLiked ? LikedIcon : LikeIcon} />
+              </span>
+            </div>
+          </div>
+
+          <div className="flex flex-col ml-auto">
+            <button
+              className="bg-blue-700 text-white p-2 rounded-sm mb-2"
+              onClick={handleEditTweet}
+            >
+              Edit Tweet
+            </button>
+            <button
+              className="bg-red-700 text-white p-2 rounded-sm"
+              onClick={handleDeleteTweet}
+            >
+              Delete Tweet
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Tweets;
