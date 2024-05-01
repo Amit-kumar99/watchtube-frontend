23c1698a2127cb12f058720bc9e653b2aa3648cd
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-05-01 12:06:43 +0530

    added History, Comments page, refactored backend

--- src/components/Pages/Comments.jsx
+++ src/components/Pages/Comments.jsx
@@ -1,5 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL_PREFIX } from "../../constants";
+
 const Comments = () => {
-  return <div>Comments</div>;
+  const [comments, setComments] = useState([]);
+
+  if (!localStorage.getItem("isLoggedIn")) {
+    return "Please log in to see your comments";
+  }
+
+  const fetchCommentsHistory = async () => {
+    const res = await axios.get(
+      `${BACKEND_URL_PREFIX}/comments/commentsHistory`,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(res.data.data);
+    if (res.data.statusCode === 200) {
+      setComments(res.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchCommentsHistory();
+  }, []);
+
+  if (!comments) {
+    return "loading...";
+  }
+
+  return (
+    <div className="w-4/12 mx-auto">
+      <h1 className="text-4xl my-5">Your Comments History</h1>
+      <div className="w-full">
+        {comments.map((comment) => (
+          <div key={comment._id} className="border mb-5 w-full">
+            <div className="bg-gray-300 p-3 rounded-lg w-full">{comment.createdAt}</div>
+            <div className="flex w-full py-3">
+              <div className="mr-5 w-7/12 flex flex-col justify-between">
+                <div className="font-semibold">{comment.content}</div>
+                <div>
+                  <span>Commented on </span>
+                  <span className="text-blue-500 font-semibold">{comment.video.title}</span>
+                </div>
+                <div>{comment.createdAt} PM/AM</div>
+              </div>
+              <div className="w-3/12"><img className="w-full h-20" src={comment.video.thumbnail} alt="thumbnail"/></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
--- src/components/Pages/History.jsx
+++ src/components/Pages/History.jsx
@@ -1,5 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL_PREFIX } from "../../constants";
+import { Link } from "react-router-dom";
+
 const History = () => {
-  return <div>History</div>;
+  const [watchedVideos, setWatchedVideos] = useState(null);
+
+  if (!localStorage.getItem("isLoggedIn")) {
+    return "Please log in to see your history";
+  }
+
+  const fetchWatchedVideos = async () => {
+    const res = await axios.get(`${BACKEND_URL_PREFIX}/users/watchHistory`, {
+      withCredentials: true,
+    });
+    console.log(res.data.data);
+    if (res.data.statusCode === 200) {
+      setWatchedVideos(res.data.data.videos);
+    }
+  };
+
+  useEffect(() => {
+    fetchWatchedVideos();
+  }, []);
+
+  if (!watchedVideos) {
+    return "loading...";
+  }
+
+  return (
+    <div className="flex w-full ml-5">
+      <div className="w-7/12 mr-10 border">
+        <h1 className="font-semibold text-3xl border mb-7">Watch History</h1>
+        <div>
+          {watchedVideos.map((video) => (
+            <Link to={`/watch?v=${video._id}`} key={video._id} className="border w-full flex mb-5">
+              <div className="w-4/12">
+                <div className="absolute bg-black text-white py-1 px-2 rounded-md">
+                  {video.duration.toFixed(2)}
+                </div>
+                <div className="mr-5">
+                  <img
+                    className="w-full h-40 rounded-lg"
+                    src={video.thumbnail}
+                    alt="thumbnail"
+                  />
+                </div>
+              </div>
+              <div className="w-8/12">
+                <div className=" mt-1 text-2xl">{video.title}</div>
+                <div className="text-gray-500 text-sm mt-1">
+                  <span className="font-semibold mr-2">{video.owner[0].username} .</span>
+                  <span className="mr-2">{video.views} views</span>
+                </div>
+                <div className="text-gray-500 text-sm mt-2 w-full">{video.description}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Link className="w-2/12" to="/comments">
+        <h1 className="font-semibold text-xl hover:text-blue-500 cursor-pointer border">
+          Comments History
+        </h1>
+      </Link>
+    </div>
+  );
 };
 
 export default History;
