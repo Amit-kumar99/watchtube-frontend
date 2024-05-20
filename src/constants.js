a3af3a34d759b650ad4d5be347b17de93115e1cd
Amit Kumar <53267975+Amit-kumar99@users.noreply.github.com>
2024-05-20 12:12:22 +0530

    Updated backend url

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL_PREFIX = "http://localhost:8000/api/v1";
+//export const BACKEND_URL_PREFIX = "http://localhost:8000/api/v1";
+export const BACKEND_URL_PREFIX = "https://watchtube.shop/api/v1";
