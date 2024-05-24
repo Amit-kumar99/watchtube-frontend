dfa3598c1b4a234c4f18c18179631d0336200594
Amit Kumar <53267975+Amit-kumar99@users.noreply.github.com>
2024-05-24 16:50:30 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 //export const BACKEND_URL_PREFIX = "http://localhost:8000/api/v1";
-export const BACKEND_URL_PREFIX = "https://watchtube.shop/api/v1";
+// export const BACKEND_URL_PREFIX = "https://watchtube.shop/api/v1";
+export const BACKEND_URL_PREFIX = "https://watchtube-backend-2.onrender.com/api/v1";
